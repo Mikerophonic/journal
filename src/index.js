85017a1c0b96a52c075bd31949c1c40bd0e15268
Mikerophonic <micgcon@gmail.com>
2023-07-16 15:36:06 -0700

    finish business logic

--- src/index.js
+++ src/index.js
@@ -11,3 +11,24 @@ Entry.prototype.numberOfWords = function() {
     });
     return wordCount;
 }
+
+Entry.prototype.vowelCounter = function() {
+    return this.body.match(/[aieou]/gi).length;
+}
+
+Entry.prototype.consonantCounter = function() {
+    return this.body.match(/[qzwsxdcrfvtgbyhnjmklp]/gi).length;
+}
+
+Entry.prototype.getTeaser = function() {
+    const sentenceArr = this.body.split(/[!.?]/);
+    const firstSentenceArr = sentenceArr[0].split(" ");
+    if  (firstSentenceArr.length <= 8) {
+        return firstSentenceArr.join(" ")}
+    else {
+        return firstSentenceArr.slice(0, 8).join(" ")
+    }
+}
+
+//user interface
+
